0d562d4de895360fa8215a594a1cd2a5ac17fb75
HuangMin <huangmin@jpush.cn>
2016-07-27 14:27:33 +0800

    rm send message route

--- Server/jbox/api_1_0/message.py
+++ Server/jbox/api_1_0/message.py
@@ -2,11 +2,11 @@ from flask import abort, Flask, jsonify, request
 from . import api
 from ..models import Developer, Integration
 
-@api.route('message/<integration_id>/<token>', method=['POST'])
-def send_message(integration_id, token):
-    developer = Integration.query.filter_by(integration_id=integration_id).first()
-    if developer is None:
-        abort(404)
-    # TODO: verify_auth_token
-    # TODO: send notification
-    return
+# @api.route('message/<integration_id>/<token>', method=['POST'])
+# def send_message(integration_id, token):
+#     developer = Integration.query.filter_by(integration_id=integration_id).first()
+#     if developer is None:
+#         abort(404)
+#     # TODO: verify_auth_token
+#     # TODO: send notification
+#     return
