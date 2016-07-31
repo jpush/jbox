f560bad9aa59c7201ee9b9cc8a1563a37e1ee255
Hevin Lau <iserlohn44@gmail.com>
2016-07-31 10:47:26 +0800

    Add sideslip delete.

--- android/JBox/app/src/main/java/com/jiguang/jbox/account/AccountFragment.java
+++ android/JBox/app/src/main/java/com/jiguang/jbox/account/AccountFragment.java
@@ -1,12 +1,16 @@
 package com.jiguang.jbox.account;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.jiguang.jbox.R;
 
 public class AccountFragment extends Fragment {
@@ -24,7 +28,32 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_account, container, false);
 
-        ListView lvAccount = (ListView) v.findViewById(R.id.lv_accounts);
+        SwipeMenuListView listView = (SwipeMenuListView) v.findViewById(R.id.lv_accounts);
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                SwipeMenuItem removeItem = new SwipeMenuItem(getActivity().getApplicationContext());
+                removeItem.setWidth(48);
+                removeItem.setTitle(R.string.account_remove);
+                removeItem.setTitleColor(Color.WHITE);
+                removeItem.setBackground(android.R.color.holo_red_light);
+
+                menu.addMenuItem(removeItem);
+            }
+        };
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                if (position == 0) { // Remove button.
+
+                }
+                return false;
+            }
+        });
+
+        listView.setMenuCreator(creator);
+        listView.setSwipeDirection(SwipeMenuListView.DIRECTION_RIGHT);
 
         return v;
     }
--- android/JBox/app/src/main/java/com/jiguang/jbox/channel/ChannelFragment.java
+++ android/JBox/app/src/main/java/com/jiguang/jbox/channel/ChannelFragment.java
@@ -1,5 +1,6 @@
 package com.jiguang.jbox.channel;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.jiguang.jbox.R;
 
 public class ChannelFragment extends Fragment {
@@ -22,6 +27,34 @@ public class ChannelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_channel, container, false);
+
+        SwipeMenuListView listView = (SwipeMenuListView) v.findViewById(R.id.lv_channel);
+        // 设置侧滑选项。
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+            @Override
+            public void create(SwipeMenu menu) {
+                SwipeMenuItem removeItem = new SwipeMenuItem(getActivity().getApplicationContext());
+                removeItem.setWidth(48);
+                removeItem.setTitle(R.string.channel_remove);
+                removeItem.setTitleColor(Color.WHITE);
+                removeItem.setBackground(android.R.color.holo_red_light);
+
+                menu.addMenuItem(removeItem);
+            }
+        };
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                if (position == 0) {    // 删除。
+
+                }
+                return false;
+            }
+        });
+
+        listView.setMenuCreator(creator);
+        listView.setSwipeDirection(SwipeMenuListView.DIRECTION_RIGHT);
 
         return v;
     }
