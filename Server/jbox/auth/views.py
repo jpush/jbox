b70d8e69a86f0ca12c437cd3b0f204adbf8f03c5
Ken <992649726@qq.com>
2016-08-09 17:18:48 +0800

    修改postToChannels

--- Server/jbox/auth/views.py
+++ Server/jbox/auth/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, redirect, request, url_for, flash, session
+import requests
+from flask import json, render_template, redirect, request, url_for, flash, session
 from flask_login import login_user, logout_user, login_required, current_user
 from wtforms import Form
 from . import auth
 from ..models import Developer
 from ..main.forms import FakeUserForm
+from ..api_1_0.developers import get_channels
 
 
 @auth.route('/logout')
@@ -29,7 +31,16 @@ def create_integration():
 @auth.route('/new/postTochannels', methods=['GET'])
 @login_required
 def post_to_channel():
-    return render_template('auth/new/postToChannels.html', dev_key=current_user.dev_key)
+    dev_key = current_user.dev_key
+    developer = Developer.query.filter_by(dev_key=dev_key).first()
+    if developer is not None:
+        channels = developer.channels
+        list = []
+        for channel in channels:
+            list.append(channel.channel)
+        return render_template('auth/new/postToChannels.html', dev_key=dev_key, channels=list)
+    else:
+        return render_template('auth/new/postToChannels.html', dev_key=dev_key)
 
 
 @auth.route('/new/channel', methods=['GET'])
