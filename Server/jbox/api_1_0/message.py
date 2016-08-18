8e5ec34bccfc662ba88f6505771f985a61028a91
Ken <992649726@qq.com>
2016-08-18 14:10:56 +0800

    message 在extra中增加 datetime 字段

--- Server/jbox/api_1_0/message.py
+++ Server/jbox/api_1_0/message.py
@@ -1,38 +1,43 @@
+import time
 from flask import abort, Flask, jsonify, request
 from . import api
 from ..models import Developer, Integration
 import jpush
 from jpush import common
 
+
 @api.route('/message/<integration_id>/<token>', methods=['POST'])
 def send_message(integration_id, token):
     if not request.json or not 'message' in request.json or not 'title' in request.json:
         abort(400)
     integration = Integration.verify_auth_token(token)
+    print(dir(integration))
     if integration is None:
         abort(404)
     # channel dev_ID
     developer = Developer.query.filter_by(id=integration.developer_id).first()
+    print(dir(developer))
     if developer is None:
         abort(404)
 
-    _jpush = jpush.JPush(u'abcacdf406411fa656ee11c3', u'682acd395df807d97e24eb50')
+    _jpush = jpush.JPush(u'a1703c14b186a68a66ef86c1', u'9dabdf8bb704b421759cb49c')
     push = _jpush.create_push()
     _jpush.set_logging("DEBUG")
     push.audience = jpush.audience(
-        jpush.tag_and(developer.dev_key, integration.channel)
+        jpush.tag(developer.dev_key + integration.channel)
     )
     # push.audience = jpush.all_
     # push.notification = jpush.notification(alert=request.json['title'],extras={'title': request.json['title'],
     #                                                                              'message': request.json['message']})
     android_msg = jpush.android(alert=request.json['title'], extras={'title': request.json['title'],
-                                                             'message': request.json['message']})
+                                                                     'message': request.json['message']})
     ios_msg = jpush.ios(alert=request.json['title'], extras={'title': request.json['title'],
                                                              'message': request.json['message']})
     # ios_msg = jpush.ios(alert=request.json['title'], extras={'title': request.json['title']})
     push.notification = jpush.notification(alert=request.json['title'], android=android_msg, ios=ios_msg)
-    push.message = jpush.message(msg_content=request.json['message'], title=request.json['title'], content_type="tyope",
-                                 extras={'dev_key': developer.dev_key, 'channel': integration.channel})
+    push.message = jpush.message(msg_content=request.json['message'], title=request.json['title'],
+                                 extras={'dev_key': developer.dev_key, 'channel': integration.channel,
+                                         'datetime': int(time.time())})
     push.options = {"time_to_live": 86400, "sendno": 12345, "apns_production": False}
     push.platform = jpush.all_
     try:
