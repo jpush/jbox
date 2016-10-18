13990c0b0fda1c55d5ae910049abac55265c939e
HuangMin <huangmin@jpush.cn>
2016-10-18 13:43:06 +0800

    remove flask_debugtoolbar

--- Server/jbox/__init__.py
+++ Server/jbox/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension
+
 from config import config
 
 bootstrap = Bootstrap()
@@ -12,7 +12,6 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'main.login'
-toolbar = DebugToolbarExtension()
 
 
 def create_app(config_name):
@@ -23,7 +22,6 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    toolbar.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
