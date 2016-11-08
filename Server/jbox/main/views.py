3f248b231d4662c11abde495deca267530f84b5c
HuangMin <huangmin@jpush.cn>
2016-11-08 17:42:20 +0800

    application\api\guide page rm redirect login

--- Server/jbox/main/views.py
+++ Server/jbox/main/views.py
@@ -154,33 +154,18 @@ def index():
 @main.route('/document', methods=['GET'])
 def document():
     developer = get_developer()
-    if developer is None:
-        return redirect(url_for('main.login'))
-    username = developer.username
-    if username is None or username == '':
-        return redirect(url_for('auth.setting'))
     return render_template('document.html', developer=developer)
 
 
 @main.route('/guide', methods=['GET'])
 def guide():
     developer = get_developer()
-    if developer is None:
-        return redirect(url_for('main.login'))
-    username = developer.username
-    if username is None or username == '':
-        return redirect(url_for('auth.setting'))
     return render_template('guide.html', developer=developer)
 
 
 @main.route('/application', methods=['GET'])
 def application():
     developer = get_developer()
-    if developer is None:
-        return redirect(url_for('main.login'))
-    username = developer.username
-    if username is None or username == '':
-        return redirect(url_for('auth.setting'))
     return render_template('application.html', developer=developer)
 
 
