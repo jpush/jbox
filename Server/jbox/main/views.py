0becdaca2855c92f9ee2c60ace3aee0a4f261d08
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2016-11-17 13:46:10 +0800

    navigator bar modify

--- Server/jbox/main/views.py
+++ Server/jbox/main/views.py
@@ -126,24 +126,6 @@ def index():
     return render_template('index.html', developer=developer)
 
 
-# @main.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = FakeUserForm()
-#     if form is None:
-#         flash('Should input username and password')
-#     else:
-#         if form.validate_on_submit():
-#             developer = Developer.query.filter_by(platform=form.platform.data,
-#                           platform_id=form.platform_id.data).first()
-#             if developer is not None:
-#                 login_user(developer, remember=True)
-#                 integrations = Developer.query.filter_by(dev_key=current_user.dev_key).first().integrations
-#                 return render_template('auth/manage.html', integrations=integrations, dev_key=current_user.dev_key)
-#             else:
-#                 flash('Can find user by platform:' + form.platform.data + ' platform_id' + form.platform_id.data)
-#     return render_template('login.html', form=form)
-
-
 @main.route('/document', methods=['GET'])
 def document():
     developer = get_developer()
