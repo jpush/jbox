6a1fb3f89fc83f0eb040226c4c515f24e2e48d6e
Hevin <iserlohn44@gmail.com>
2017-03-21 17:31:45 +0800

    Optimize code style & support debug / product environment.

--- android/JBox/app/src/main/java/com/jiguang/jbox/main/MainActivity.java
+++ android/JBox/app/src/main/java/com/jiguang/jbox/main/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsListView;
@@ -21,6 +20,7 @@ import com.jiguang.jbox.data.Developer;
 import com.jiguang.jbox.data.Message;
 import com.jiguang.jbox.drawer.ChannelListFragment;
 import com.jiguang.jbox.drawer.NavigationDrawerFragment;
+import com.jiguang.jbox.util.LogUtil;
 import com.jiguang.jbox.util.PermissionUtil;
 import com.jiguang.jbox.view.TopBar;
 
@@ -29,238 +29,223 @@ import java.util.List;
 import cn.jpush.android.api.JPushInterface;
 
 public class MainActivity extends FragmentActivity
-        implements ChannelListFragment.OnListFragmentInteractionListener,
-        NavigationDrawerFragment.OnDrawerPageChangeListener {
+    implements ChannelListFragment.OnListFragmentInteractionListener,
+    NavigationDrawerFragment.OnDrawerPageChangeListener {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+  private static final String TAG = LogUtil.makeLogTag(MainActivity.class);
 
-    public static final int MSG_WHAT_RECEIVE_MSG_CURRENT = 0;
-    public static final int MSG_WHAT_RECEIVE_MSG = 1;
-    public static final int MSG_WHAT_UPDATE_DEV = 2;
-    public static final int MSG_WHAT_OPEN_MSG = 3;
+  public static final int MSG_WHAT_RECEIVE_MSG_CURRENT = 0;
+  public static final int MSG_WHAT_RECEIVE_MSG = 1;
+  public static final int MSG_WHAT_UPDATE_DEV = 2;
+  public static final int MSG_WHAT_OPEN_MSG = 3;
 
-    private final int QUERY_MESSAGE_COUNT = 10;
+  private final int QUERY_MESSAGE_COUNT = 10;
 
-    private TopBar mTopBar;
-    private ListView mMsgListView;
-    private MessageListAdapter mAdapter;
+  private TopBar mTopBar;
+  private ListView mMsgListView;
+  private MessageListAdapter mAdapter;
 
-    private List<Message> mMessages;
+  private List<Message> mMessages;
 
-    private NavigationDrawerFragment mDrawerFragment;
+  private NavigationDrawerFragment mDrawerFragment;
 
-    private DrawerLayout mDrawerLayout;
+  private DrawerLayout mDrawerLayout;
 
-    private int mCurrentOffset = 0; // 当前加载的消息数。
+  private int mCurrentOffset = 0; // 当前加载的消息数。
 
-    public static Handler handler;
+  public static Handler handler;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+      @Override public void onDrawerSlide(View drawerView, float slideOffset) {
 
-            }
+      }
 
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
-            }
+      @Override public void onDrawerOpened(View drawerView) {
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+      }
 
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-            }
+      @Override public void onDrawerClosed(View drawerView) {
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+      }
 
-            @Override
-            public void onDrawerStateChanged(int newState) {
+      @Override public void onDrawerStateChanged(int newState) {
 
-            }
-        });
+      }
+    });
 
-        // 点击除开侧边栏的区域会收起侧边栏。
-        mDrawerLayout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        Log.i(TAG, "mDrawerLayout touch down");
-                        mDrawerLayout.closeDrawers();
-                        break;
-                }
-                return false;
-            }
-        });
-
-        mDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.navigation_drawer);
-
-        mTopBar = (TopBar) findViewById(R.id.topBar);
-        mTopBar.setTitle(AppApplication.currentChannelName);
-
-        ImageView ivMenu = (ImageView) findViewById(R.id.iv_menu);
-        ivMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mDrawerFragment.getView() != null) {
-                    mDrawerLayout.openDrawer(mDrawerFragment.getView());
-                }
-            }
-        });
-
-        if (!PermissionUtil.hasPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            PermissionUtil.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, 0);
+    // 点击除开侧边栏的区域会收起侧边栏。
+    mDrawerLayout.setOnTouchListener(new View.OnTouchListener() {
+      @Override public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+          case MotionEvent.ACTION_DOWN:
+            LogUtil.LOGI(TAG, "mDrawerLayout touch down");
+            mDrawerLayout.closeDrawers();
+            break;
         }
+        return false;
+      }
+    });
 
-        handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(android.os.Message message) {
-                Bundle data = message.getData();
+    mDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(
+        R.id.navigation_drawer);
 
-                if (message.what == MSG_WHAT_RECEIVE_MSG_CURRENT) {
-                    // 收到的是当前 Channel 的消息，更新界面。
-                    Message msg = (Message) data.getSerializable("message");
-                    mAdapter.addMessage(msg);
-                    mCurrentOffset++;
+    mTopBar = (TopBar) findViewById(R.id.topBar);
+    mTopBar.setTitle(AppApplication.currentChannelName);
 
-                } else if (message.what == MSG_WHAT_RECEIVE_MSG) {
-                    String devKey = data.getString("DevKey");
-                    mDrawerFragment.devListFragment.updateData();
-                    mDrawerFragment.channelListFragment.updateData(devKey);
-
-                } else if (message.what == MSG_WHAT_UPDATE_DEV) {
-                    String devKey = data.getString("DevKey");
-                    mDrawerFragment.devListFragment.updateData();
-                    mDrawerFragment.channelListFragment.updateData(devKey);
-                }
-                return false;
-            }
-        });
-
-        mMsgListView = (ListView) findViewById(R.id.lv_msg);
-        mMsgListView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView absListView, int i) {
-
-            }
-
-            @Override
-            public void onScroll(AbsListView absListView, int firstVisibleItem,
-                                 int visibleItemCount, int totalItemCount) {
-                // 如果滑动到了最后，加载更多数据。
-                if (mMsgListView.getLastVisiblePosition() + 1 == totalItemCount) {
-                    if (mMessages == null) {
-                        return;
-                    }
-                    List<Message> newMessageList = queryMessages(AppApplication.currentDevKey,
-                            AppApplication.currentChannelName, mMessages.size(),
-                            QUERY_MESSAGE_COUNT);
-                    if (newMessageList != null && !newMessageList.isEmpty()) {
-                        mMessages.addAll(newMessageList);
-                        mCurrentOffset += newMessageList.size();
-                        mAdapter.replaceData(mMessages);
-                    }
-                }
-            }
-        });
-
-        View emptyView = findViewById(R.id.tv_hint);
-        mMsgListView.setEmptyView(emptyView);
-
-        mMessages = queryMessages(AppApplication.currentDevKey, AppApplication.currentChannelName,
-                0, QUERY_MESSAGE_COUNT);
-        mCurrentOffset = mMessages.size();
-        mAdapter = new MessageListAdapter(this, mMessages);
-        mMsgListView.setAdapter(mAdapter);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        JPushInterface.onResume(this);
-        if (AppApplication.shouldUpdateData) {
-            setMessages(AppApplication.currentDevKey, AppApplication.currentChannelName);
-            mDrawerFragment.updateData();
-            AppApplication.shouldUpdateData = false;
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        JPushInterface.onPause(this);
-    }
-
-    @Override
-    public void onBackPressed() {
+    ImageView ivMenu = (ImageView) findViewById(R.id.iv_menu);
+    ivMenu.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
         if (mDrawerFragment.getView() != null) {
-            if (mDrawerLayout.isDrawerOpen(mDrawerFragment.getView())) {
-                mDrawerLayout.closeDrawers();
-                return;
-            }
+          mDrawerLayout.openDrawer(mDrawerFragment.getView());
         }
-        super.onBackPressed();
+      }
+    });
+
+    if (!PermissionUtil.hasPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+      PermissionUtil.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, 0);
     }
 
-    @Override
-    public void onChannelListItemClick(Channel channel) {
-        mTopBar.setTitle(channel.name);
+    handler = new Handler(new Handler.Callback() {
+      @Override public boolean handleMessage(android.os.Message message) {
+        Bundle data = message.getData();
 
-        // Team 栏对应图标上减去未读消息数。
-        if (channel.unreadCount != 0) {
-            Developer dev = new Select().from(Developer.class)
-                    .where("Key=?", channel.devKey)
-                    .executeSingle();
-            if (dev.unreadCount - channel.unreadCount >= 0) {
-                dev.unreadCount -= channel.unreadCount;
-            } else {
-                dev.unreadCount = 0;
-            }
-            dev.save();
-            mDrawerFragment.devListFragment.updateData();
+        if (message.what == MSG_WHAT_RECEIVE_MSG_CURRENT) {
+          // 收到的是当前 Channel 的消息，更新界面。
+          Message msg = (Message) data.getSerializable("message");
+          mAdapter.addMessage(msg);
+          mCurrentOffset++;
+        } else if (message.what == MSG_WHAT_RECEIVE_MSG) {
+          String devKey = data.getString("DevKey");
+          mDrawerFragment.devListFragment.updateData();
+          mDrawerFragment.channelListFragment.updateData(devKey);
+        } else if (message.what == MSG_WHAT_UPDATE_DEV) {
+          String devKey = data.getString("DevKey");
+          mDrawerFragment.devListFragment.updateData();
+          mDrawerFragment.channelListFragment.updateData(devKey);
         }
+        return false;
+      }
+    });
 
-        channel.unreadCount = 0;
-        channel.save();
-        mDrawerFragment.channelListFragment.updateData(channel.devKey);
+    mMsgListView = (ListView) findViewById(R.id.lv_msg);
+    mMsgListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+      @Override public void onScrollStateChanged(AbsListView absListView, int i) {
 
-        List<Message> messages = queryMessages(channel.devKey, channel.name, 0,
-                mCurrentOffset + QUERY_MESSAGE_COUNT);
-        mAdapter.replaceData(messages);
-        mAdapter.notifyDataSetChanged();
+      }
 
-        if (mDrawerFragment.getView() != null) {
-            mDrawerLayout.closeDrawer(mDrawerFragment.getView());
+      @Override
+      public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount,
+          int totalItemCount) {
+        // 如果滑动到了最后，加载更多数据。
+        if (mMsgListView.getLastVisiblePosition() + 1 == totalItemCount) {
+          if (mMessages == null) {
+            return;
+          }
+          List<Message> newMessageList =
+              queryMessages(AppApplication.currentDevKey, AppApplication.currentChannelName,
+                  mMessages.size(), QUERY_MESSAGE_COUNT);
+          if (newMessageList != null && !newMessageList.isEmpty()) {
+            mMessages.addAll(newMessageList);
+            mCurrentOffset += newMessageList.size();
+            mAdapter.replaceData(mMessages);
+          }
         }
-        JPushInterface.clearAllNotifications(getApplicationContext());
+      }
+    });
+
+    View emptyView = findViewById(R.id.tv_hint);
+    mMsgListView.setEmptyView(emptyView);
+
+    mMessages = queryMessages(AppApplication.currentDevKey, AppApplication.currentChannelName, 0,
+        QUERY_MESSAGE_COUNT);
+    mCurrentOffset = mMessages.size();
+    mAdapter = new MessageListAdapter(this, mMessages);
+    mMsgListView.setAdapter(mAdapter);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    JPushInterface.onResume(this);
+    if (AppApplication.shouldUpdateData) {
+      setMessages(AppApplication.currentDevKey, AppApplication.currentChannelName);
+      mDrawerFragment.updateData();
+      AppApplication.shouldUpdateData = false;
+    }
+  }
+
+  @Override protected void onPause() {
+    super.onPause();
+    JPushInterface.onPause(this);
+  }
+
+  @Override public void onBackPressed() {
+    if (mDrawerFragment.getView() != null) {
+      if (mDrawerLayout.isDrawerOpen(mDrawerFragment.getView())) {
+        mDrawerLayout.closeDrawers();
+        return;
+      }
+    }
+    super.onBackPressed();
+  }
+
+  @Override public void onChannelListItemClick(Channel channel) {
+    mTopBar.setTitle(channel.name);
+
+    // Team 栏对应图标上减去未读消息数。
+    if (channel.unreadCount != 0) {
+      Developer dev =
+          new Select().from(Developer.class).where("Key=?", channel.devKey).executeSingle();
+
+      if (dev.unreadCount - channel.unreadCount >= 0) {
+        dev.unreadCount -= channel.unreadCount;
+      } else {
+        dev.unreadCount = 0;
+      }
+      dev.save();
+      mDrawerFragment.devListFragment.updateData();
     }
 
-    @Override
-    public void onPageSelected(boolean isLast) {
-        if (isLast) {
-            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-        } else if (mDrawerLayout.getDrawerLockMode(GravityCompat.START) == DrawerLayout.LOCK_MODE_UNLOCKED){
-            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
-        }
-    }
+    channel.unreadCount = 0;
+    channel.save();
+    mDrawerFragment.channelListFragment.updateData(channel.devKey);
 
-    private void setMessages(String devKey, String channelName) {
-        mMessages = queryMessages(devKey, channelName, 0, mCurrentOffset + QUERY_MESSAGE_COUNT);
-        mAdapter.replaceData(mMessages);
-        mTopBar.setTitle(channelName);
-    }
+    List<Message> messages =
+        queryMessages(channel.devKey, channel.name, 0, mCurrentOffset + QUERY_MESSAGE_COUNT);
+    mAdapter.replaceData(messages);
+    mAdapter.notifyDataSetChanged();
 
-    private List<Message> queryMessages(String devKey, String channelName, int offSet, int limit) {
-        return new Select().from(Message.class)
-                .where("DevKey=? AND Channel=?", devKey, channelName)
-                .offset(offSet)
-                .limit(limit)
-                .orderBy("time DESC")
-                .execute();
+    if (mDrawerFragment.getView() != null) {
+      mDrawerLayout.closeDrawer(mDrawerFragment.getView());
     }
+    JPushInterface.clearAllNotifications(getApplicationContext());
+  }
+
+  @Override public void onPageSelected(boolean isLast) {
+    if (isLast) {
+      mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+    } else if (mDrawerLayout.getDrawerLockMode(GravityCompat.START)
+        == DrawerLayout.LOCK_MODE_UNLOCKED) {
+      mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN);
+    }
+  }
+
+  private void setMessages(String devKey, String channelName) {
+    mMessages = queryMessages(devKey, channelName, 0, mCurrentOffset + QUERY_MESSAGE_COUNT);
+    mAdapter.replaceData(mMessages);
+    mTopBar.setTitle(channelName);
+  }
+
+  private List<Message> queryMessages(String devKey, String channelName, int offSet, int limit) {
+    return new Select().from(Message.class)
+        .where("DevKey=? AND Channel=?", devKey, channelName)
+        .offset(offSet)
+        .limit(limit)
+        .orderBy("time DESC")
+        .execute();
+  }
 }
--- android/JBox/app/src/main/java/com/jiguang/jbox/main/MessageListAdapter.java
+++ android/JBox/app/src/main/java/com/jiguang/jbox/main/MessageListAdapter.java
@@ -19,127 +19,121 @@ import com.jiguang.jbox.data.Message;
 
 import java.util.List;
 
-
 class MessageListAdapter extends BaseAdapter {
 
-    private List<Message> mMessages;
-    private Context mContext;
+  private List<Message> mMessages;
+  private Context mContext;
 
-    MessageListAdapter(Context context, List<Message> list) {
-        mMessages = list;
-        mContext = context;
+  MessageListAdapter(Context context, List<Message> list) {
+    mMessages = list;
+    mContext = context;
+  }
+
+  void replaceData(List<Message> list) {
+    if (list == null) {
+      mMessages.clear();
+    } else {
+      mMessages = list;
+    }
+    notifyDataSetChanged();
+  }
+
+  void addMessage(Message msg) {
+    if (mMessages != null) {
+      mMessages.add(0, msg);
+      notifyDataSetChanged();
+    }
+  }
+
+  @Override public int getCount() {
+    return mMessages.size();
+  }
+
+  @Override public Object getItem(int position) {
+    return mMessages.get(position);
+  }
+
+  @Override public long getItemId(int position) {
+    return position;
+  }
+
+  @Override public View getView(int position, View convertView, ViewGroup parent) {
+    ViewHolder holder;
+    if (convertView == null) {
+      LayoutInflater inflater = LayoutInflater.from(mContext);
+      convertView = inflater.inflate(R.layout.item_msg, parent, false);
+      holder = new ViewHolder();
+      holder.tvIcon = (TextView) convertView.findViewById(R.id.tv_icon);
+      holder.ivIcon = (ImageView) convertView.findViewById(R.id.iv_icon);
+      holder.tvTitle = (TextView) convertView.findViewById(R.id.tv_title);
+      holder.tvContent = (TextView) convertView.findViewById(R.id.tv_content);
+      holder.tvTime = (TextView) convertView.findViewById(R.id.tv_time);
+      convertView.setTag(holder);
+    } else {
+      holder = (ViewHolder) convertView.getTag();
     }
 
-    void replaceData(List<Message> list) {
-        if (list == null) {
-            mMessages.clear();
-        } else {
-            mMessages = list;
+    Message msg = (Message) getItem(position);
+    final String url = msg.url;
+
+    convertView.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        if (!TextUtils.isEmpty(url)) {
+          Intent intent = new Intent(Intent.ACTION_VIEW);
+          intent.setData(Uri.parse(url));
+          mContext.startActivity(intent);
         }
-        notifyDataSetChanged();
+      }
+    });
+
+    if (TextUtils.isEmpty(msg.iconUrl)) {
+      if (holder.ivIcon.getVisibility() == View.VISIBLE) {
+        holder.ivIcon.setVisibility(View.INVISIBLE);
+        holder.tvIcon.setVisibility(View.VISIBLE);
+      }
+
+      String firstChar;
+      if (TextUtils.isEmpty(msg.integrationName)) {
+        firstChar = msg.channelName.substring(0, 1).toUpperCase();
+      } else {
+        firstChar = msg.integrationName.substring(0, 1).toLowerCase();
+      }
+      holder.tvIcon.setText(firstChar);
+    } else {
+      if (holder.ivIcon.getVisibility() == View.INVISIBLE) {
+        holder.ivIcon.setVisibility(View.VISIBLE);
+        holder.tvIcon.setVisibility(View.GONE);
+      }
+      Glide.with(AppApplication.getAppContext())
+          .load("http://" + msg.iconUrl)
+          .centerCrop()
+          .placeholder(R.drawable.default_avatar)
+          .into(holder.ivIcon);
     }
 
-    void addMessage(Message msg) {
-        if (mMessages != null) {
-            mMessages.add(0, msg);
-            notifyDataSetChanged();
-        }
+    if (TextUtils.isEmpty(msg.title)) {
+      holder.tvTitle.setVisibility(View.GONE);
+    } else {
+      if (holder.tvTime.getVisibility() == View.GONE) {
+        holder.tvTitle.setVisibility(View.VISIBLE);
+      }
+      holder.tvTitle.setText(msg.title);
     }
 
-    @Override
-    public int getCount() {
-        return mMessages.size();
-    }
+    holder.tvContent.setText(msg.content);
 
-    @Override
-    public Object getItem(int position) {
-        return mMessages.get(position);
-    }
+    String formatTime =
+        DateUtils.formatDateTime(parent.getContext(), msg.time * 1000, DateUtils.FORMAT_SHOW_TIME);
+    holder.tvTime.setText(formatTime);
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+    return convertView;
+  }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-        if (convertView == null) {
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            convertView = inflater.inflate(R.layout.item_msg, parent, false);
-            holder = new ViewHolder();
-            holder.tvIcon = (TextView) convertView.findViewById(R.id.tv_icon);
-            holder.ivIcon = (ImageView) convertView.findViewById(R.id.iv_icon);
-            holder.tvTitle = (TextView) convertView.findViewById(R.id.tv_title);
-            holder.tvContent = (TextView) convertView.findViewById(R.id.tv_content);
-            holder.tvTime = (TextView) convertView.findViewById(R.id.tv_time);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        Message msg = (Message) getItem(position);
-        final String url = msg.url;
-
-        convertView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!TextUtils.isEmpty(url)) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(url));
-                    mContext.startActivity(intent);
-                }
-            }
-        });
-
-        if (TextUtils.isEmpty(msg.iconUrl)) {
-            if (holder.ivIcon.getVisibility() == View.VISIBLE) {
-                holder.ivIcon.setVisibility(View.INVISIBLE);
-                holder.tvIcon.setVisibility(View.VISIBLE);
-            }
-
-            String firstChar;
-            if (TextUtils.isEmpty(msg.integrationName)) {
-                firstChar = msg.channelName.substring(0, 1).toUpperCase();
-            } else {
-                firstChar = msg.integrationName.substring(0, 1).toLowerCase();
-            }
-            holder.tvIcon.setText(firstChar);
-        } else {
-            if (holder.ivIcon.getVisibility() == View.INVISIBLE) {
-                holder.ivIcon.setVisibility(View.VISIBLE);
-                holder.tvIcon.setVisibility(View.GONE);
-            }
-            Glide.with(AppApplication.getAppContext())
-                    .load("http://" + msg.iconUrl)
-                    .centerCrop()
-                    .placeholder(R.drawable.default_avatar)
-                    .into(holder.ivIcon);
-        }
-
-        if (TextUtils.isEmpty(msg.title)) {
-            holder.tvTitle.setVisibility(View.GONE);
-        } else {
-            if (holder.tvTime.getVisibility() == View.GONE) {
-                holder.tvTitle.setVisibility(View.VISIBLE);
-            }
-            holder.tvTitle.setText(msg.title);
-        }
-
-        holder.tvContent.setText(msg.content);
-
-        String formatTime = DateUtils.formatDateTime(parent.getContext(),
-                msg.time * 1000, DateUtils.FORMAT_SHOW_TIME);
-        holder.tvTime.setText(formatTime);
-
-        return convertView;
-    }
-
-    private static class ViewHolder {
-        TextView tvIcon;
-        ImageView ivIcon;
-        TextView tvTitle;
-        TextView tvContent;
-        TextView tvTime;
-    }
+  private static class ViewHolder {
+    TextView tvIcon;
+    ImageView ivIcon;
+    TextView tvTitle;
+    TextView tvContent;
+    TextView tvTime;
+  }
 }
