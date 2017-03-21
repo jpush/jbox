1c9517cd7b35cb5fc2aa27e4ba8d782be46934d7
Hevin <iserlohn44@gmail.com>
2017-03-21 17:31:45 +0800

    Optimize code style & support debug / product environment.

--- android/JBox/app/src/main/java/com/jiguang/jbox/drawer/NavigationDrawerFragment.java
+++ android/JBox/app/src/main/java/com/jiguang/jbox/drawer/NavigationDrawerFragment.java
@@ -1,6 +1,5 @@
 package com.jiguang.jbox.drawer;
 
-
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,112 +13,105 @@ import android.widget.ImageView;
 import com.jiguang.jbox.AppApplication;
 import com.jiguang.jbox.R;
 import com.jiguang.jbox.drawer.adapter.DrawerViewPagerAdapter;
-import com.jiguang.jbox.main.MainActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.jiguang.jbox.R.id.viewPager;
 
-
 /**
  * 侧滑界面。
  */
 public class NavigationDrawerFragment extends Fragment
-        implements DeveloperListFragment.OnListFragmentInteractionListener {
+    implements DeveloperListFragment.OnListFragmentInteractionListener {
 
-    private ViewPager mViewPager;
+  private ViewPager mViewPager;
 
-    private ImageView mIvCircleFirst;
-    private ImageView mIvCircleSecond;
+  private ImageView mIvCircleFirst;
+  private ImageView mIvCircleSecond;
 
-    public DeveloperListFragment devListFragment;
-    public ChannelListFragment channelListFragment;
+  public DeveloperListFragment devListFragment;
+  public ChannelListFragment channelListFragment;
 
-    public NavigationDrawerFragment() {
-    }
+  public NavigationDrawerFragment() {
+  }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
+  @Override public void onAttach(Context context) {
+    super.onAttach(context);
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.drawer_main, container, false);
+  @Override public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+      Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.drawer_main, container, false);
 
-        mIvCircleFirst = (ImageView) v.findViewById(R.id.iv_circle_first);
-        mIvCircleSecond = (ImageView) v.findViewById(R.id.iv_circle_second);
-        mIvCircleFirst.setSelected(true);
+    mIvCircleFirst = (ImageView) v.findViewById(R.id.iv_circle_first);
+    mIvCircleSecond = (ImageView) v.findViewById(R.id.iv_circle_second);
+    mIvCircleFirst.setSelected(true);
 
-        final List<Fragment> fragmentList = new ArrayList<>(2);
+    final List<Fragment> fragmentList = new ArrayList<>(2);
 
-        devListFragment = new DeveloperListFragment();
-        devListFragment.setListener(this);
-        fragmentList.add(devListFragment);
+    devListFragment = new DeveloperListFragment();
+    devListFragment.setListener(this);
+    fragmentList.add(devListFragment);
 
-        channelListFragment = new ChannelListFragment();
-        fragmentList.add(channelListFragment);
+    channelListFragment = new ChannelListFragment();
+    fragmentList.add(channelListFragment);
 
-        final OnDrawerPageChangeListener drawerPageChangeListener = (MainActivity) getActivity();
+    FragmentManager fm = getActivity().getSupportFragmentManager();
 
-        FragmentManager fm = getActivity().getSupportFragmentManager();
+    mViewPager = (ViewPager) v.findViewById(viewPager);
+    mViewPager.setAdapter(new DrawerViewPagerAdapter(fm, fragmentList));
 
-        mViewPager = (ViewPager) v.findViewById(viewPager);
-        mViewPager.setAdapter(new DrawerViewPagerAdapter(fm, fragmentList));
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    final OnDrawerPageChangeListener drawerPageChangeListener =
+        (OnDrawerPageChangeListener) getActivity();
+    mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+      @Override
+      public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-            }
+      }
 
-            @Override
-            public void onPageSelected(int position) {
-                if (position == fragmentList.size() - 1) {
-                    drawerPageChangeListener.onPageSelected(true);
-                } else {
-                    drawerPageChangeListener.onPageSelected(false);
-                }
+      @Override public void onPageSelected(int position) {
+        if (position == fragmentList.size() - 1) {
+          drawerPageChangeListener.onPageSelected(true);
+        } else {
+          drawerPageChangeListener.onPageSelected(false);
+        }
 
-                if (0 == position) {
-                    mIvCircleFirst.setSelected(true);
-                    mIvCircleSecond.setSelected(false);
-                } else {
-                    mIvCircleFirst.setSelected(false);
-                    mIvCircleSecond.setSelected(true);
-                }
-            }
+        if (0 == position) {
+          mIvCircleFirst.setSelected(true);
+          mIvCircleSecond.setSelected(false);
+        } else {
+          mIvCircleFirst.setSelected(false);
+          mIvCircleSecond.setSelected(true);
+        }
+      }
 
-            @Override
-            public void onPageScrollStateChanged(int state) {
+      @Override public void onPageScrollStateChanged(int state) {
 
-            }
-        });
-        return v;
-    }
+      }
+    });
+    return v;
+  }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
+  @Override public void onDetach() {
+    super.onDetach();
+  }
 
-    @Override
-    public void onDevListItemClick(String devKey) {
-        channelListFragment.updateData(devKey);
-        mViewPager.setCurrentItem(1);
-    }
+  @Override public void onDevListItemClick(String devKey) {
+    channelListFragment.updateData(devKey);
+    mViewPager.setCurrentItem(1);
+  }
 
-    public void updateData() {
-        devListFragment.updateData();
-        channelListFragment.updateData(AppApplication.currentDevKey);
-    }
+  public void updateData() {
+    devListFragment.updateData();
+    channelListFragment.updateData(AppApplication.currentDevKey);
+  }
 
-    /**
-     * 监听侧边栏的页面选择
-     */
-    public interface OnDrawerPageChangeListener {
+  /**
+   * 监听侧边栏的页面选择
+   */
+  public interface OnDrawerPageChangeListener {
 
-        void onPageSelected(boolean isLast);
-    }
+    void onPageSelected(boolean isLast);
+  }
 }
--- android/JBox/app/src/main/java/com/jiguang/jbox/view/TopBar.java
+++ android/JBox/app/src/main/java/com/jiguang/jbox/view/TopBar.java
@@ -14,70 +14,67 @@ import com.jiguang.jbox.R;
 
 public class TopBar extends FrameLayout {
 
-    private TextView mTvTitle;
-    private TextView mTvLeft;
-    private TextView mTvRight;
+  private TextView mTvTitle;
+  private TextView mTvLeft;
+  private TextView mTvRight;
 
-    public TopBar(Context context) {
-        super(context);
+  public TopBar(Context context) {
+    super(context);
+  }
+
+  public TopBar(Context context, AttributeSet attrs) {
+    super(context, attrs);
+
+    LayoutInflater.from(context).inflate(R.layout.topbar, this);
+
+    TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.TopBar, 0, 0);
+
+    String title = a.getString(R.styleable.TopBar_topbar_title);
+    String rightText = a.getString(R.styleable.TopBar_right);
+    boolean isShowLeft = a.getBoolean(R.styleable.TopBar_show_left, false);
+    boolean isShowRight = a.getBoolean(R.styleable.TopBar_show_right, false);
+    int bgLeft = a.getResourceId(R.styleable.TopBar_background_left, 0);
+
+    mTvLeft = (TextView) findViewById(R.id.tv_left);
+    if (bgLeft != 0) {
+      mTvLeft.setText(null);
+      mTvLeft.setBackgroundResource(bgLeft);
     }
 
-    public TopBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-
-        LayoutInflater.from(context).inflate(R.layout.topbar, this);
-
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.TopBar, 0, 0);
-
-        String title = a.getString(R.styleable.TopBar_topbar_title);
-        String rightText = a.getString(R.styleable.TopBar_right);
-        boolean isShowLeft = a.getBoolean(R.styleable.TopBar_show_left, false);
-        boolean isShowRight = a.getBoolean(R.styleable.TopBar_show_right, false);
-        int bgLeft = a.getResourceId(R.styleable.TopBar_background_left, 0);
-
-        mTvLeft = (TextView) findViewById(R.id.tv_left);
-        if (bgLeft != 0) {
-            mTvLeft.setText(null);
-            mTvLeft.setBackgroundResource(bgLeft);
+    if (isShowLeft) {
+      mTvLeft.setVisibility(View.VISIBLE);
+      mTvLeft.setOnClickListener(new OnClickListener() {
+        @Override public void onClick(View v) {
+          ((Activity) getContext()).finish();
         }
-
-        if (isShowLeft) {
-            mTvLeft.setVisibility(View.VISIBLE);
-            mTvLeft.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ((Activity) getContext()).finish();
-                }
-            });
-        }
-
-        mTvTitle = (TextView) findViewById(R.id.tv_title);
-        mTvTitle.setText(title);
-
-        mTvRight = (TextView) findViewById(R.id.tv_right);
-        if (isShowRight) {
-            mTvRight.setVisibility(View.VISIBLE);
-        }
-        if (!TextUtils.isEmpty(rightText)) {
-            mTvRight.setText(rightText);
-        }
-
+      });
     }
 
-    public TopBar(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+    mTvTitle = (TextView) findViewById(R.id.tv_title);
+    mTvTitle.setText(title);
 
-    public void setTitle(String title) {
-        mTvTitle.setText(title);
+    mTvRight = (TextView) findViewById(R.id.tv_right);
+    if (isShowRight) {
+      mTvRight.setVisibility(View.VISIBLE);
     }
-
-    public void setRightClick(OnClickListener listener) {
-        mTvRight.setOnClickListener(listener);
+    if (!TextUtils.isEmpty(rightText)) {
+      mTvRight.setText(rightText);
     }
+  }
 
-    public void setLeftClick(OnClickListener listener) {
-        mTvLeft.setOnClickListener(listener);
-    }
+  public TopBar(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
 
+  public void setTitle(String title) {
+    mTvTitle.setText(title);
+  }
+
+  public void setRightClick(OnClickListener listener) {
+    mTvRight.setOnClickListener(listener);
+  }
+
+  public void setLeftClick(OnClickListener listener) {
+    mTvLeft.setOnClickListener(listener);
+  }
 }
